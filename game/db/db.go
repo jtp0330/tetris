763e3c6d903b8b1ef85718af398eb6a335ac70f2
Jared Park <jtp0330@gmail.com>
2023-09-02 17:59:08 -0700

    new changes, modifying gui code, will need to run on full version of linux as will not work on wsl

--- game/db/db.go
+++ game/db/db.go
@@ -1,6 +1,20 @@
 package db
 
+import (
+	"fmt"
+
+	"github.com/lib/pq"
+)
+
 // connect to user_data postgres to record user data, and score in postgres docker container
 func Postgres_Connect() {
-	print("Successfully connected to Postgres DB")
+	// connect to postgres db
+	pgUrl, err := pq.ParseURL("postgres://postgres:postgres@localhost:5432/user_data?sslmode=disable")
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Print("Successfully connected to Postgres DB: {}", pgUrl)
+
 }
